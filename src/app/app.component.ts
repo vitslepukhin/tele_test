ea4b2d16b8812440303aadf73a5f5e7e5a851b71
Vitaliy Slepukhin <vitslepukin@gmail.com>
2024-06-29 15:53:07 +0400

    add simple button with notific

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,38 @@
-import { NgDompurifySanitizer } from "@tinkoff/ng-dompurify";
-import { TuiRootModule, TuiDialogModule, TuiAlertModule, TUI_SANITIZER } from "@taiga-ui/core";
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {
+  TUI_SANITIZER,
+  TuiAlertModule,
+  TuiAlertService,
+  TuiButtonModule,
+  TuiDialogModule,
+  TuiRootModule,
+} from '@taiga-ui/core';
+import { NgDompurifySanitizer } from '@tinkoff/ng-dompurify';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, TuiRootModule, TuiDialogModule, TuiAlertModule],
+  imports: [
+    RouterOutlet,
+    TuiRootModule,
+    TuiDialogModule,
+    TuiAlertModule,
+    TuiButtonModule,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
-    providers: [{provide: TUI_SANITIZER, useClass: NgDompurifySanitizer}]
+  providers: [{ provide: TUI_SANITIZER, useClass: NgDompurifySanitizer }],
 })
 export class AppComponent {
   title = 'tele-test';
+  constructor(
+    @Inject(TuiAlertService) private readonly alerts: TuiAlertService
+  ) {}
+
+  showNotification(): void {
+    this.alerts
+      .open('Basic <strong>HTML</strong>', { label: 'With a heading!' })
+      .subscribe();
+  }
 }
