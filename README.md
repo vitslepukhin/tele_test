05e535710b2651c116f9f3a8c86067bac9db6125
Vitaliy Slepukhin <vitslepukin@gmail.com>
2024-06-29 14:40:22 +0400

    switch to scss

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# TeleTest
+# Tele
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 18.0.6.
 
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'tele-test' title`, () => {
+  it(`should have the 'tele' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('tele-test');
+    expect(app.title).toEqual('tele');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, tele-test');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, tele');
   });
 });
